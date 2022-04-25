4021008fac97518d01fde06a5539f7eec97cc28c
Austin Melendez <AustinJames17@icloud.com>
2022-04-25 14:50:16 -0400

    add next seo instead of og content

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,21 +11,26 @@ import styles from '../styles/Home.module.scss';
 const Home: NextPage = () => {
   return (
     <>
-      <Head>
-        <title>Home | Emily Wills</title>
-        <meta name="description" content="Emily Wills is an experience designer, entrepreneur, and empath from Virginia, US." />
-        <meta property="og:url" content="https://www.emilywills.com/" />
-        <meta property="og:title" content="Home | Emily Wills" />
-        <meta property="og:description" content="This page will allow you to link your Discord account to your MetaMask Wallet for WL and other future features." />
-        <meta property="og:image" content="https://res.cloudinary.com/austinmel/image/upload/v1650911928/ogimage1_ggyamz.jpg" />
-
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta property="twitter:domain" content="emilywills.com" />
-        <meta property="twitter:url" content="https://www.emilywills.com/" />
-        <meta name="twitter:title" content="Hon's Buns - Wallet Connect" />
-        <meta name="twitter:description" content="Emily Wills is an experience designer, entrepreneur, and empath from Virginia, US." />
-        <meta name="twitter:image" content="https://res.cloudinary.com/austinmel/image/upload/v1650911928/ogimage1_ggyamz.jpg" />
-      </Head>
+      <NextSeo
+        title="Home | Emily Wills"
+        description="Emily Wills is an experience designer, entrepreneur, and empath from Virginia, US."
+        defaultTitle="Welcome to Hon's Buns"
+        canonical="https://www.emilywills.com/"
+        openGraph={{
+          url: 'https://www.emilywills.com/',
+          title: "Emily Wills' Portfolio",
+          description: "Emily Wills is an experience designer, entrepreneur, and empath from Virginia, US.",
+          images: [
+            { url: 'https://res.cloudinary.com/austinmel/image/upload/v1650911928/ogimage1_ggyamz.jpg' }
+          ],
+          site_name: 'EmilyWills',
+        }}
+      // twitter={{
+      //   handle: '@honsbuns',
+      //   site: '@honsbuns',
+      //   cardType: 'summary_large_image',
+      // }}
+      />
       <main className="container">
         <HomeHero />
         <IntroContent />
