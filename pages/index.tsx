b500dd61b1397e568f5fe2ca903255a0de901beb
Austin Melendez <AustinJames17@icloud.com>
2022-04-25 14:27:45 -0400

    update seo and og content

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,21 +11,23 @@ import styles from '../styles/Home.module.scss';
 const Home: NextPage = () => {
   return (
     <>
-      <NextSeo
-        title="Home | Emily Wills"
-        description="Emily Wills is an experience designer, entrepreneur, and empath from Virginia, US."
-        defaultTitle="Welcome to my portfolio"
-        canonical="https://www.emilywills.com/"
-        openGraph={{
-          url: 'https://www.emilywills.com/',
-          title: "Emily Wills",
-          description: "Emily Wills is an experience designer, entrepreneur, and empath from Virginia, US.",
-          images: [
-            { url: 'https://res.cloudinary.com/austinmel/image/upload/v1650909161/Screen_Shot_2022-04-25_at_1.47.41_PM_i3pwxd.png' }
-          ],
-          site_name: 'EmilyWills',
-        }}
-      />
+      <Head>
+        <title>Home | Emily Wills</title>
+        <meta name="description" content="Emily Wills is an experience designer, entrepreneur, and empath from Virginia, US." />
+        <meta property="og:url" content="https://www.honsbuns.com/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Home | Emily Wills" />
+        <meta property="og:description" content="This page will allow you to link your Discord account to your MetaMask Wallet for WL and other future features." />
+        <meta property="og:image" content="https://res.cloudinary.com/austinmel/image/upload/v1650909161/Screen_Shot_2022-04-25_at_1.47.41_PM_i3pwxd.png" />
+
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="emilywills.com" />
+        <meta property="twitter:url" content="https://www.emilywills.com/" />
+        <meta name="twitter:title" content="Hon's Buns - Wallet Connect" />
+        <meta name="twitter:description" content="Emily Wills is an experience designer, entrepreneur, and empath from Virginia, US." />
+        <meta name="twitter:image" content="https://res.cloudinary.com/austinmel/image/upload/v1650909161/Screen_Shot_2022-04-25_at_1.47.41_PM_i3pwxd.png" />
+      </Head>
       <main className="container">
         <HomeHero />
         <IntroContent />
