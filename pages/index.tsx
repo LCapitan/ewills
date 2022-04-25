065dd7beab7e84b53d8728eee8e9fd046c8316f5
Austin Melendez <AustinJames17@icloud.com>
2022-04-25 14:39:19 -0400

    update og content

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,7 @@ const Home: NextPage = () => {
         <meta property="og:type" content="website" />
         <meta property="og:title" content="Home | Emily Wills" />
         <meta property="og:description" content="This page will allow you to link your Discord account to your MetaMask Wallet for WL and other future features." />
-        <meta property="og:image" content="https://res.cloudinary.com/austinmel/image/upload/v1650909161/Screen_Shot_2022-04-25_at_1.47.41_PM_i3pwxd.png" />
+        <meta property="og:image" content="https://res.cloudinary.com/austinmel/image/upload/v1650911928/ogimage1_ggyamz.jpg" />
 
 
         <meta name="twitter:card" content="summary_large_image" />
@@ -26,7 +26,7 @@ const Home: NextPage = () => {
         <meta property="twitter:url" content="https://www.emilywills.com/" />
         <meta name="twitter:title" content="Hon's Buns - Wallet Connect" />
         <meta name="twitter:description" content="Emily Wills is an experience designer, entrepreneur, and empath from Virginia, US." />
-        <meta name="twitter:image" content="https://res.cloudinary.com/austinmel/image/upload/v1650909161/Screen_Shot_2022-04-25_at_1.47.41_PM_i3pwxd.png" />
+        <meta name="twitter:image" content="https://res.cloudinary.com/austinmel/image/upload/v1650911928/ogimage1_ggyamz.jpg" />
       </Head>
       <main className="container">
         <HomeHero />
