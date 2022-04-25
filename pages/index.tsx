11fad83d3b670d69d6bc4d4290cc63582ca26a39
Austin Melendez <AustinJames17@icloud.com>
2022-04-25 14:46:38 -0400

    fix og stuff

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,11 +15,9 @@ const Home: NextPage = () => {
         <title>Home | Emily Wills</title>
         <meta name="description" content="Emily Wills is an experience designer, entrepreneur, and empath from Virginia, US." />
         <meta property="og:url" content="https://www.emilywills.com/" />
-        <meta property="og:type" content="website" />
         <meta property="og:title" content="Home | Emily Wills" />
         <meta property="og:description" content="This page will allow you to link your Discord account to your MetaMask Wallet for WL and other future features." />
         <meta property="og:image" content="https://res.cloudinary.com/austinmel/image/upload/v1650911928/ogimage1_ggyamz.jpg" />
-
 
         <meta name="twitter:card" content="summary_large_image" />
         <meta property="twitter:domain" content="emilywills.com" />
